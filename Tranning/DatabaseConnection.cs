6c9f263a5cab3c9ff64e5fdfead2571dd39211c6
jetaime19 <hoangdzung03@gmail.com>
2023-12-09 23:38:35 +0700

    upload file

---
+++ Tranning/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Tranning
+{
+    public class DatabaseConnection
+    {
+        public DatabaseConnection() { }
+
+        public static SqlConnection GetSqlConnection()
+        {
+            string connectionString = "Server=CTINH\\DUNG;Database=Tranning;Trusted_Connection=True;TrustServerCertificate=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+
+    }
+}
